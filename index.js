fbd8453425b39bd27cbb8a76acee29f3346640ad
Ken-muigai <kenmuigai980@gmail.com>
2020-08-04 15:36:50 +0300

    succeding commits

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+var size = $('#home input[name=size]:checked  ').val();
+var crust = $(' input[name=crust]:checked ').val();
+var topping = $('#menu2 input[name=topping]:checked').val();
+var deliver = $('#selection option:selected').val();
+
+function grandTotal(m,n,o,p){
+    return m + n + o + p
+}
+
+
 $(document).ready(function(){
     $('#submission').click(function(e){
         e.preventDefault();
-        console.log($('#home input[name=size]:checked  ').val());
-        console.log($(' input[name=crust]:checked ').val());
-        console.log($('#menu2 input[name=topping]:checked').val());
-        console.log($('#selection:selected').text())
+        console.log(grandTotal(size,crust,topping,deliver))
     })
 })
