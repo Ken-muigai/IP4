101a7206054d8bd6130c828cfaaadc4034856a58
Ken-muigai <kenmuigai980@gmail.com>
2020-08-04 15:53:58 +0300

    succeding commits

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-var size = parseInt($('#home input[name=size]:checked').val());
-var crust = parseInt($(' input[name=crust]:checked ').val());
-var topping = parseInt($('#menu2 input[name=topping]:checked').val());
-var deliver = parseInt($('#selection option:selected').val());
-
 function grandTotal(m,n,o,p){
-    return m + n + o + p
+    var q = parseInt(m);
+    var r = parseInt(n);
+    var s = parseInt(o);
+    var t = parseInt(p)
+    return q + r + s + t
 }
 
 
 $(document).ready(function(){
     $('#submission').click(function(e){
         e.preventDefault();
+        var size = $('#home input[name=size]:checked').val();
+        var crust = $(' input[name=crust]:checked ').val();
+        var topping = $('#menu2 input[name=topping]:checked').val();
+        var deliver = $('#selection option:selected').val();
         console.log(grandTotal(size,crust,topping,deliver))
     })
 })
