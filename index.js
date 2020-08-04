97c7c8e8e5e66f1504ed45b1b728d9f700d0f9dd
Ken-muigai <kenmuigai980@gmail.com>
2020-08-04 14:06:40 +0300

    succeding commits

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#submission').click(function(e){
+        e.preventDefault();
+        console.log($('#home input[name=size]:checked  ').val());
+        console.log($(' input[name=crust]:checked ').val());
+        console.log($('#menu2 input[name=topping]:checked').val());
+        console.log($('input#divone').checked.val())
+    })
+})
