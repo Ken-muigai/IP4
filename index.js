620adb45465e58c37735c807297f711e4f42eb52
Ken-muigai <kenmuigai980@gmail.com>
2020-08-05 13:33:12 +0300

    succeeding commits

--- index.js
+++ index.js
@@ -10,20 +10,25 @@ function grandTotal(m,n,o,p){
 $(document).ready(function(){
     $('#submission').click(function(e){
         e.preventDefault();
+
         var size = parseInt($('#home input[name=size]:checked').val());
         var crust = parseInt($('#crust input[name=crust]:checked ').val());
         var topping = parseInt($('#topping input[name=topping]:checked').val());
         var deliver = $('#selection option:selected').val();
+
         if(deliver){
-            alert(`We have receive your order of TOTAL: ${grandTotal(size,crust,topping,deliver)} check receipt at the bottom.Gracia.`);
+            alert(`We have received your order of TOTAL: ${grandTotal(size,crust,topping,deliver)} check receipt at the bottom.Gracia.`);
         }else{
             alert(size + crust + topping)
         }
 
         $('.receipt #holder').append(`
-        <li> Size: ${size}</li>,
-        <li> Crust for ${crust}</li>,
-        <li> Topping for ${topping}</li>`)
+        <h2>Receipt</h2>
+        <li> SIZE KSH. ${size}</li>
+        <li> CRUST KSH. ${crust}</li>
+        <li> TOPPING KSH. ${topping}</li>
+        <li> DELIVERY KSH. ${deliver}</li>
+        <h6>FIRST ORDER TOTAL = Ksh. ${grandTotal(size,crust,topping,deliver)}</h6>`)
     })
 })
 
