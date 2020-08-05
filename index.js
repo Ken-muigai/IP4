e8913e632e4a299f511a02fe0e5eb107d1f7373f
Ken-muigai <kenmuigai980@gmail.com>
2020-08-05 13:08:44 +0300

    succeeding commits

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ function grandTotal(m,n,o,p){
 $(document).ready(function(){
     $('#submission').click(function(e){
         e.preventDefault();
-        var size = $('#home input[name=size]:checked').val();
-        var crust = $('#crust input[name=crust]:checked ').val();
-        var topping = $('#topping input[name=topping]:checked').val();
+        var size = parseInt($('#home input[name=size]:checked').val());
+        var crust = parseInt($('#crust input[name=crust]:checked ').val());
+        var topping = parseInt($('#topping input[name=topping]:checked').val());
         var deliver = $('#selection option:selected').val();
-        alert(`We have receive your order of TOTAL: ${grandTotal(size,crust,topping,deliver)} check receipt at the bottom.Gracia.`);
+        if(deliver){
+            alert(`We have receive your order of TOTAL: ${grandTotal(size,crust,topping,deliver)} check receipt at the bottom.Gracia.`);
+        }else{
+            alert(size + crust + topping)
+        }
     })
 })
 
